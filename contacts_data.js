e3b195a3cdd169c037473e7e53e2d183945ed615
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-19 18:48:42 +0530

    Initial Commit for file: contacts_data.js

---
+++ contacts_data.js
@@ -0,0 +1,26 @@
+export const item = {
+    "contact_list": [
+        {
+            "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/692.png",
+            "id": "2f071ac8-52f2-46e4-8ad4-453117ae6286",
+            "contact_name": "tend",
+            "contact_status": "perhaps",
+            "chat": false
+        },
+        {
+            "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/500.png",
+            "id": "5c73479e-776b-41c7-b7a3-3cb0539d08fc",
+            "contact_name": "course",
+            "contact_status": "hotel",
+            "chat": true
+        },
+        {
+            "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png",
+            "id": "ba86c323-ef2d-460e-8088-ef49fd0cbbb7",
+            "contact_name": "weight",
+            "contact_status": "management",
+            "chat": true
+        }
+    ],
+    "id": "c8a9d2ed-fe2e-41f1-8bc6-7234bbcb6704"
+}
