a6efe6b5711214ca1f3265a3ae99b5de6103c49c
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-05-19 18:48:43 +0530

    Initial Commit for file: data.js

---
+++ data.js
@@ -0,0 +1,41 @@
+export const data = {
+    "c8a9d2ed-fe2e-41f1-8bc6-7234bbcb6704": {
+        "id": "c8a9d2ed-fe2e-41f1-8bc6-7234bbcb6704",
+        "index": 0,
+        "created_at": "05/19/2023 18:47:36",
+        "created_by": ""
+    },
+    "2f071ac8-52f2-46e4-8ad4-453117ae6286": {
+        "id": "2f071ac8-52f2-46e4-8ad4-453117ae6286",
+        "parent_id": "c8a9d2ed-fe2e-41f1-8bc6-7234bbcb6704",
+        "index": 0,
+        "created_at": "05/19/2023 18:47:36",
+        "created_by": "",
+        "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/692.png",
+        "contact_name": "tend",
+        "contact_status": "perhaps",
+        "chat": false
+    },
+    "5c73479e-776b-41c7-b7a3-3cb0539d08fc": {
+        "id": "5c73479e-776b-41c7-b7a3-3cb0539d08fc",
+        "parent_id": "c8a9d2ed-fe2e-41f1-8bc6-7234bbcb6704",
+        "index": 0,
+        "created_at": "05/19/2023 18:47:36",
+        "created_by": "",
+        "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/500.png",
+        "contact_name": "course",
+        "contact_status": "hotel",
+        "chat": true
+    },
+    "ba86c323-ef2d-460e-8088-ef49fd0cbbb7": {
+        "id": "ba86c323-ef2d-460e-8088-ef49fd0cbbb7",
+        "parent_id": "c8a9d2ed-fe2e-41f1-8bc6-7234bbcb6704",
+        "index": 0,
+        "created_at": "05/19/2023 18:47:36",
+        "created_by": "",
+        "profile_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/94.png",
+        "contact_name": "weight",
+        "contact_status": "management",
+        "chat": true
+    }
+}
